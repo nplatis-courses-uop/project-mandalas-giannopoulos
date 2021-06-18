527e2148c6b7bf47445a10395b82a93dcc0b0cf5
Stefanos Mandalas <sealmove@protonmail.com>
2021-06-18 03:51:44 +0300

    sent info to server

--- Client/src/main/java/client/App.java
+++ Client/src/main/java/client/App.java
@@ -1,9 +1,21 @@
 package client;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.List;
+
+import org.javatuples.Pair;
 
 public class App {
     public static void send(String plate, List<String> services) {
-
+        try (
+            var socket = new Socket("localhost", 5555);
+            var objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        ) {
+            objectOutputStream.writeObject(Pair.with(plate, services));
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 }
