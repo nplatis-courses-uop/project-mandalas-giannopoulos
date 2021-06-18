44edcc9e2ce2d2b502db260c45ddac5c2e254b94
Stefanos Mandalas <sealmove@protonmail.com>
2021-06-18 03:51:56 +0300

    created table

--- Server/src/main/java/server/Server.java
+++ Server/src/main/java/server/Server.java
@@ -1,22 +1,28 @@
 package server;
 
+import java.util.List;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- * JavaFX App
- */
 public class Server extends Application {
-
+    TableView<List<String>> table = new TableView<>();
     @Override
     public void start(Stage stage) {
-        var label = new Label("Hello, JavaFX Server");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        table.getColumns().add(makeCol("Plate"));
+        table.getColumns().add(makeCol("Cost"));
+        table.getColumns().add(makeCol("Time of arrival"));
+        var mainPane = new StackPane(table);
+        var scene = new Scene(mainPane, 640, 480);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static TableColumn<List<String>, String> makeCol(String label) {
+        return new TableColumn<List<String>, String>(label);
     }
 
     public static void main(String[] args) {
