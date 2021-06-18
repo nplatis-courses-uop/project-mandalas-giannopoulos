773cee7fa3223e21721bc213aee0b9bc08fbbfda
Kwtsos <57370817+kostasdj@users.noreply.github.com>
2021-06-19 02:14:38 +0300

    Improved OrderServer API and we added some buttons

--- Server/src/main/java/server/OrderServer.java
+++ Server/src/main/java/server/OrderServer.java
@@ -4,15 +4,27 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class OrderServer implements Runnable {
+    private ServerSocket serverSocket;
+
     @Override
     public void run() {
-        try (var serverSocket = new ServerSocket(5555)) {
-            while (true) {
+        try {
+            serverSocket = new ServerSocket(5555);
+            while (Server.isRunning) {
                 var clientSocket = serverSocket.accept();
                 new Thread(new OrderProcessor(clientSocket)).start();
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    public void shutdown() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 }
--- Server/src/main/java/server/Server.java
+++ Server/src/main/java/server/Server.java
@@ -4,23 +4,41 @@ import java.util.List;
 import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Server extends Application {
+    public static boolean isRunning = true;
     public static TableView<List<String>> table = new TableView<>();
+    private static OrderServer server = new OrderServer();
+
     @Override
     public void start(Stage stage) {
         table.getColumns().add(makeCol(0, "Plate"));
         table.getColumns().add(makeCol(1, "Cost"));
         table.getColumns().add(makeCol(2, "Time of arrival"));
-        var mainPane = new StackPane(table);
-        var scene = new Scene(mainPane, 640, 480);
+        
+        var buttonPane = new HBox();
+        var registerBtn = new Button("Register");
+        var deleteBtn = new Button("Delete");
+        buttonPane.getChildren().addAll(registerBtn, deleteBtn);
+        
+        var mainPane = new VBox(table, buttonPane);
+        var scene = new Scene(mainPane, 1280, 720);
         stage.setScene(scene);
+        stage.setTitle("Server");
         stage.show();
-        new Thread(new OrderServer()).start();
+        new Thread(server).start();
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+        server.shutdown();
     }
 
     public static TableColumn<List<String>, String> makeCol(int index, String label) {
