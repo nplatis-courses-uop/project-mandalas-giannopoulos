db4f4dc2896b057afb68e28caf54b76d91e80821
Stefanos Mandalas <sealmove@protonmail.com>
2021-06-21 04:18:53 +0300

    specified fonts

--- Client/src/main/java/client/Client.java
+++ Client/src/main/java/client/Client.java
@@ -9,7 +9,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
